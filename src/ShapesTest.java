a3eac96d704c71cf6a57a2e6b5f3b57ca71b341d
lisbethcollin <lisbeth.collin@hotmail.com>
2021-04-27 11:09:26 +0200

    Add test

---
+++ src/ShapesTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShapesTest extends Shape {
+
+    @Test
+    void testGetArea() {
+    }
+
+    @Test
+    void testGetCircumference() {
+    }
+
+    @Test
+    void testGetCenter() {
+    }
+
+    @Test
+    void testIsPointInside() {
+    }
+
+    @Test
+    void testGetEuclideanDist() {
+    }
+}
